4a714746ff5e105c1a6449599b512f866f0b100a
mugoh <mugoh.ks@gmail.com>
2019-09-22 20:00:03 +0300

    serialize env variables

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,10 +1,27 @@
 //! Environment Variables Config
 //!
 use envy;
-use serde::Deserialize;
+use serde::{self, Deserialize};
+use std::error;
 
 /// Holds Env variables deserialized to a struct
 #[derive(Deserialize, Debug)]
 pub struct Config {
-    rust_log: String = "actix-web=info",
+    #[serde(default = "default_log")]
+    pub rust_log: String,
+    pub secret_key: String,
+}
+
+/// Default ENV value for log
+fn default_log() -> String {
+    String::from("actix-web=info")
+}
+
+/// Returns a Serialized ENV variable Configuration
+/// struct
+pub fn get_env_config() -> Result<Config, Box<dyn error::Error>> {
+    match envy::from_env::<Config>() {
+        Ok(c) => Ok(c),
+        Err(e) => Result::Err(Box::new(e)),
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod apps;
+pub mod config;
 
 #[macro_use(lazy_static)]
 extern crate lazy_static;
