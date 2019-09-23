be34710030c16634816ffb98505efa66f0b8bed9
mugoh <mugoh.ks@gmail.com>
2019-09-23 16:20:03 +0300

    include custom responses

--- src/apps/user/views.rs
+++ src/apps/user/views.rs
@@ -2,27 +2,39 @@
 //!
 
 use crate::apps::auth::validate;
+use crate::apps::core::response;
 use crate::apps::user::models::User;
-use actix_web::{http, web, HttpResponse};
 
+use actix_web::{http, web, HttpResponse};
 use validator::Validate;
 
 /// Registers a new user
 ///
-/// # method
-///
+/// # methods
+/// - ## POST
 ///
 /// # Returns
-/// JSON of received User data
+/// - On Sucess: JSONResponse
+/// - On ERROR: JSONErrResponse
+///
 pub fn register_user(data: web::Json<User>) -> HttpResponse {
-    // jwt
     let user_ = data.0.clone();
     let token = validate::encode_jwt_token(user_).unwrap();
     let _claims = validate::decode_auth_token(&token);
 
     if let Err(err) = data.validate() {
-        return HttpResponse::build(http::StatusCode::BAD_REQUEST).json(err);
+        let res = response::JsonErrResponse::new(http::StatusCode::BAD_REQUEST.to_string(), err);
+        return HttpResponse::build(http::StatusCode::BAD_REQUEST).json(&res);
         // Filter json where message is not null
     };
-    HttpResponse::build(http::StatusCode::CREATED).json(data.0)
+    let res = response::JsonResponse::new(
+        http::StatusCode::CREATED.to_string(),
+        format!(
+            "Success. An activation link sent to {}",
+            &data.0.email.clone().unwrap()
+        ),
+        data.0.clone(),
+    );
+
+    HttpResponse::build(http::StatusCode::CREATED).json(&res)
 }
