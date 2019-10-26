a4e56bbbb2d7ecb8cf4fe71556330fd31a731627
mugoh <mugoh.ks@gmail.com>
2019-10-26 22:19:18 +0300

    (user): add upload script and calling interface

--- src/core/derived.rs
+++ src/core/derived.rs
@@ -8,3 +8,31 @@ macro_rules! hashmap {
     $(map.insert($key, $val);)*
         map
 }}}
+
+/// Creates a Python module object
+///
+/// Python is used to call the Cloudinary API
+/// for local-file upload
+///
+/// Sounds lame :(, but  a direct call to the cloudinary API
+/// can't be used in upload of local files
+fn create_py_mod() -> Result(()) {
+    let gil = Python::acquire_gil();
+    gil.python?
+}
+
+/// Perfoms the foreign call to the script that should
+/// execute the upload
+///
+/// # Arguments
+/// file: &str
+///     - The url path of the file to upload
+///
+pub fn <'a>upload_static(file: &'a str) -> Result(()) {
+    use py03::PyModule;
+    use std::fs;
+
+    let script = fs::read_to_string("scr/core/upload")?;
+    let loaded_mod = PyModule::from_code(create_py_mod(), script, "upload", "upload")?;
+    loaded_mod.call("upload", (file, ),None).extract()?
+}
