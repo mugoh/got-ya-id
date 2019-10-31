f4a02ef80c15a17c46ab8c3ab312b3f19be4a4b4
mugoh <mugoh.ks@gmail.com>
2019-10-31 19:28:05 +0300

    (prf): serialize upload bytes

--- src/apps/profiles/utils.rs
+++ src/apps/profiles/utils.rs
@@ -1,13 +1,14 @@
 //! Common functions for the Profiles module
 
-use futures::future::{err, Either};
+use futures::future::{err, lazy, Either};
 use futures::{Future, Stream};
 
 use actix_multipart::{Field, MultipartError};
+use actix_rt::System;
 use actix_web::{client, error as act_err, web, Error};
 
 use dotenv::dotenv;
-use serde_json::json;
+use serde::{Deserialize, Serialize};
 
 use std::{env, error, fs::File, io::Write};
 
@@ -35,27 +36,35 @@ pub fn extract_multipart_field<'a>(field: Field) -> impl Future<Item = i64, Erro
                 //
                 web::block(move || {
                     //
-                    let client = client::Client::default();
                     let upload_url =
                         env::var("UPLOAD_URL").expect("ENV Err: Missing static image upload URL");
-                    client
-                        .post(upload_url)
-                        .header("User-Agent", "Got ya Id")
-                        .send_json(&json!({
-                            "file": bytes.as_ref(),
-                            "upload_preset": "avatar"
-                        }))
-                        .map_err(|e| println!("{:?}", e))
-                        .and_then(|response| {
-                            println!("response: {:?}", response);
-                            Ok(())
-                        });
 
                     file.write_all(bytes.as_ref()).map_err(|e| {
                         println!("File.write failed: {:?}", e);
                         MultipartError::Payload(act_err::PayloadError::Io(e))
                     })?;
                     acc += bytes.len() as i64;
+                    // println!("bytes: {:?}", bytes);
+
+                    System::new("upload")
+                        .block_on(lazy(|| {
+                            //
+                            let data = UploadData {
+                                file: bytes.as_ref().to_vec(),
+                                upload_preset: "avatar".to_string(),
+                            };
+                            let client = client::Client::default();
+                            client
+                                .post(upload_url)
+                                .header("User-Agent", "Got ya Id")
+                                .send_json(&data)
+                                .map_err(|e| println!("{:?}", e))
+                                .and_then(|response| {
+                                    println!("response: {:?}", response);
+                                    Ok(())
+                                })
+                        }))
+                        .expect("File uploading failed");
                     Ok((file, acc))
                 })
                 .map_err(|err: act_err::BlockingError<MultipartError>| {
@@ -86,4 +95,31 @@ fn make_temp_file<'a>() -> Result<(File, String), Box<dyn error::Error>> {
     let f_path = dir.to_str().unwrap().to_string();
     let f = File::create(&dir)?;
     Ok((f, f_path))
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct UploadData {
+    #[serde(with = "base_64")]
+    file: Vec<u8>,
+    upload_preset: String,
+}
+
+mod base_64 {
+    use base64;
+    use serde::{de, Deserialize, Deserializer, Serializer};
+
+    pub fn serialize<S>(bytes: &[u8], serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&base64::encode(bytes))
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let str_ = <&str>::deserialize(deserializer)?;
+        base64::decode(str_).map_err(de::Error::custom)
+    }
 }
