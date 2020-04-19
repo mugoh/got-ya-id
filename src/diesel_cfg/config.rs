5ad8aa4a7e1cc2b03a737d096fb48b5c8b8376a9
mugoh <mugoh.ks@gmail.com>
2020-04-19 14:17:28 +0300

    use lazy db url

--- src/diesel_cfg/config.rs
+++ src/diesel_cfg/config.rs
@@ -7,26 +7,28 @@ use std::collections::HashMap;
 use std::env;
 
 lazy_static! {
-//
+    static ref DB_URL: String = {
+        dotenv().ok();
+        let keys = vec!["db_username", "db_pass", "db_host", "db_port", "db_name"];
+        let mut db_vars: HashMap<&str, String> = HashMap::new();
+
+        for key in &keys {
+            db_vars.insert(
+                key,
+                env::var(key).expect(&format!("Missing Database config variable: {}", key)),
+            );
+        }
+
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            db_vars.get(&"db_username").unwrap(),
+            db_vars.get(&"db_pass").unwrap(),
+            db_vars.get(&"db_host").unwrap(),
+            db_vars.get(&"db_port").unwrap(),
+            db_vars.get(&"db_name").unwrap()
+        )
+    };
 }
 pub fn connect_to_db() -> PgConnection {
-    dotenv().ok();
-    let keys = vec!["db_username", "db_pass", "db_host", "db_port", "db_name"];
-    let mut db_vars: HashMap<&str, String> = HashMap::new();
-
-    for key in &keys {
-        db_vars.insert(
-            key,
-            env::var(key).expect(&format!("Missing Dabase config variable: {}", key)),
-        );
-    }
-    let db_url = format!(
-        "postgres://{}:{}@{}:{}/{}",
-        db_vars.get(&"db_username").unwrap(),
-        db_vars.get(&"db_pass").unwrap(),
-        db_vars.get(&"db_host").unwrap(),
-        db_vars.get(&"db_port").unwrap(),
-        db_vars.get(&"db_name").unwrap()
-    );
-    PgConnection::establish(&db_url).expect(&format!("Error connecting to {}", &db_url))
+    PgConnection::establish(&DB_URL).expect("Error Initializing the database connection")
 }
