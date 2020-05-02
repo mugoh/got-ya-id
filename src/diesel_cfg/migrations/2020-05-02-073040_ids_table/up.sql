4e4b8d5cae64127934704cd7d3d624696d00f062
mugoh <mugoh.ks@gmail.com>
2020-05-02 11:49:23 +0300

    create ids table

---
+++ src/diesel_cfg/migrations/2020-05-02-073040_ids_table/up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE identifications (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    course VARCHAR NOT NULL,
+    valid_from DATE,
+    valid_till DATE,
+    institution VARCHAR NOT NULL,
+    campus VARCHAR, /*institution location*/
+    location_name VARCHAR NOT NULL,
+    location_point POINT, /*lat/longitude -> Use this with map api*/
+    picture VARCHAR,
+    posted_by INTEGER REFERENCES users (id),
+    is_found BOOLEAN DEFAULT FALSE
+);
