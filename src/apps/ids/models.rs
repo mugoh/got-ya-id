1c6f05db3fbe55a88b69b6d2666140709ada7453
mugoh <mugoh.ks@gmail.com>
2020-05-03 09:42:05 +0300

    deserialize dates to naivedate

--- src/apps/ids/models.rs
+++ src/apps/ids/models.rs
@@ -1,17 +1,16 @@
 //! Identification card models
 
 use super::utils::serde_pg_point;
-use crate::apps::user::utils::to_year;
 use crate::diesel_cfg::{config::connect_to_db, schema::identifications};
 
-use chrono::{NaiveDate, NaiveDateTime};
+use chrono::NaiveDate;
 use diesel::{self, prelude::*};
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 use validator_derive::Validate;
 
 use diesel_geometry::data_types::PgPoint;
-use std::borrow::Cow;
+use std::{borrow::Cow, error::Error as stdErr};
 /// Represents the Queryable IDentification data model
 /// matching the database `identification` schema
 #[derive(Queryable, Serialize, Deserialize, Identifiable)]
@@ -20,14 +19,15 @@ pub struct Identification {
     pub id: i32,
     pub name: String,
     pub course: String,
-    #[serde(deserialize_with = "to_year")]
-    pub valid_from: NaiveDateTime,
-    #[serde(deserialize_with = "to_year")]
-    pub valid_till: NaiveDateTime,
+    pub valid_from: Option<NaiveDate>,
+    pub valid_till: Option<NaiveDate>,
     pub institution: String,
     pub campus: Option<String>,
     pub location_name: String,
-    pub location_point: Option<(f64, f64)>,
+
+    #[serde(flatten, with = "serde_pg_point")]
+    pub location_point: PgPoint,
+
     pub picture: Option<String>,
     posted_by: Option<i32>,
     is_found: bool,
@@ -47,4 +47,15 @@ pub struct NewIdentification<'a> {
     location_name: Cow<'a, str>,
     #[serde(flatten, with = "serde_pg_point")]
     location_point: PgPoint,
+}
+
+impl<'a> NewIdentification<'a> {
+    /// Saves a new ID record to the Identifications table
+    pub fn save(&mut self) -> Result<Identification, Box<dyn stdErr>> {
+        //
+        let ID = diesel::insert_into(identifications::table)
+            .values(&*self)
+            .get_result::<Identification>(&connect_to_db())?;
+        Ok(ID)
+    }
 }
