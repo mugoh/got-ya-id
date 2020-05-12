54eca92381d6d3caa6ce386c300185b733cf8d69
mugoh <mugoh.ks@gmail.com>
2020-05-12 16:32:09 +0300

    add claimed idts table

---
+++ src/diesel_cfg/migrations/2020-05-12-130849_create_claimable_query/down.sql
@@ -0,0 +1,1 @@
+DROP TABLE claimed_identifications
---
+++ src/diesel_cfg/migrations/2020-05-12-130849_create_claimable_query/up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE claimed_identifications (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users (id) ON DELETE CASCADE,
+    name VARCHAR NOT NULL,
+    course VARCHAR NOT NULL,
+    entry_year DATE,
+    graduation_year DATE,
+    institution VARCHAR NOT NULL,
+    campus_location VARCHAR NOT NULL,
+    created_at timestamp without time zone not null default (now() at time zone 'utc'),
+    updated_at timestamp without time zone not null default (now() at time zone 'utc')
+);
+
+SELECT diesel_manage_updated_at('claimed_identifications');
